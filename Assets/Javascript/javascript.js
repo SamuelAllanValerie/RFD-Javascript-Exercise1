40030669899840a7334e121885fb4fec2eab89c9
Samuel Allan Valerie <17672@csmrouen.net>
2019-02-04 15:23:12 +0100

    liste imbiquee

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -1,9 +1,22 @@
 var resultat = "<ul>";
+var compteur = 0;
 
-for (var i = 0 ; i <= 100 ; i++) {
- resultat = resultat + "<li>" + i + "</li>";
+for (var i = -12728 ; i < 16856 ; i += 17) {
+  if (i % 23 == 0) {
+ resultat += "<li>" + i + "</li>";
+ // resultat = resultat + "<li>" + i + "</li>" ;
+  compteur++;
+
+  resultat += "<ul>";
+  for (var j = 0 ; j < 11 ; j++) {
+    resultat += "<li>" + j + "</li>";
+  }
+  resultat += "<ul>";
+  resultat += "</ul>";
+ }
 }
 
-resultat = resultat + "</ul>" ;
+resultat += "</ul>" ;
 
 document.write(resultat);
+alert(compteur);
