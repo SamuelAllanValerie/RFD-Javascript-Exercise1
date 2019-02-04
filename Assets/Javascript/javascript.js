8b89b060a7d7673e5f5bf0e7d9c519a096b5dc12
Samuel Allan Valerie <17672@csmrouen.net>
2019-02-04 11:54:26 +0100

    pop up message

---
+++ Assets/Javascript/javascript.js
@@ -0,0 +1,12 @@
+var message = prompt('Veuillez saisir une valeur');
+var reponse = "";
+
+if (message < 0) {
+  reponse = 'Nombre negatif';
+}
+
+ else {
+  reponse = 'nombre positif';
+ }
+
+ alert(reponse);
